5f283c75cd6db0f32b07f35a19e9ed2c81686b91
joshrmorris2 <moofase1@gmail.com>
2024-01-25 23:17:30 -0800

    lesson02 complete need to test

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const lesson1Controller = require('../controllers/lesson1');
 routes.get('/', lesson1Controller.johnRoute);
 routes.get('/jane', lesson1Controller.janeRoute);
 routes.get('/joe', lesson1Controller.joeRoute);
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
