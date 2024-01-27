cba0e595329ba004d9884e062be97544798f91ed
joshrmorris2 <moofase1@gmail.com>
2024-01-26 20:19:49 -0800

    altered error catching

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,11 +4,10 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res, next) => {
     try {
         const result = await mongodb.getDb().db().collection('contacts').find();
-        result.toArray().then((lists) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
-    } catch {
+        const lists = await result.toArray();
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -18,11 +17,10 @@ const getSingle = async (req, res, next) => {
     try {
         const userId = new ObjectId(req.params.id);
         const result = await mongodb.getDb().db().collection('contacts').find({_id: userId});
-        result.toArray().then((lists) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
-        });
-    } catch {
+        const lists = await result.toArray();
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
