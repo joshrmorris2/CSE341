4f5bb9ac640ad5c3c36c981e3e160266b4163756
joshrmorris2 <moofase1@gmail.com>
2024-01-30 22:54:13 -0800

    Lesson 03 complete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -9,7 +10,6 @@ const getAll = async (req, res, next) => {
             res.status(200).json(lists);
         });
     } catch {
-        console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
@@ -23,12 +23,81 @@ const getSingle = async (req, res, next) => {
             res.status(200).json(lists[0]);
         });
     } catch {
-        console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
+const create = async (req, res, next) => {
+    try {
+        const contact = {
+            _id: new ObjectId(),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday,
+        }
+        await mongodb
+            .getDb().db().collection('contacts').insertOne(contact)
+            .then(result => {
+                console.log(result);
+                console.log(result._id)
+                // res.redirect('/'); // This is after testing that the console.log worked.
+            });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const response = await mongodb.getDb().db().collection('contacts').updateOne(
+            { _id: new ObjectId(req.params.id) }, 
+            {
+                $set: {            
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    favoriteColor: req.body.favoriteColor,
+                    birthday: req.body.birthday,
+                },
+            });
+            if(response.modifiedCount > 0) {
+                res.status(200).json({ message: 'Update successful' });
+            } else {
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+};
+
+const remove = async (req, res, next) => {
+    try {
+        const result = await mongodb.getDb().db().collection('contacts').deleteOne(
+            { _id: new ObjectId(req.params.id) }, true);
+
+        if (result.deletedCount > 0 ) {
+            res.status(200).json({ message: 'Update successful' });
+        } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    create,
+    update,
+    remove
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,12 @@
 const routes = require('express').Router();
 const contactController = require('../controllers/contacts');
 
+routes.post('/', contactController.create);
+
 routes.get('/', contactController.getAll);
 routes.get('/:id', contactController.getSingle);
+
+routes.put('/:id', contactController.update);
+routes.delete('/:id', contactController.remove);
 
 module.exports = routes;
