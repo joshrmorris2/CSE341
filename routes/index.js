cf539628ef45fe5997d3d67baee5925c0c04c879
joshrmorris2 <moofase1@gmail.com>
2024-02-03 20:32:00 -0800

    removed lesson 1 names (John Smith, etc.)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
 
-routes.get('/', lesson1Controller.johnRoute);
-routes.get('/jane', lesson1Controller.janeRoute);
-routes.get('/joe', lesson1Controller.joeRoute);
 routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
